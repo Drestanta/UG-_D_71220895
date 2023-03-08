29b17125593b90b849ef3a733ee6cedf0c12656e
Drestanta <118432795+Drestanta@users.noreply.github.com>
2023-03-08 18:42:50 +0700

    Update 1_D_71220895.py

--- 1_D_71220895.py
+++ 1_D_71220895.py
@@ -1,30 +1,3 @@
-# def ganti_kata(kalimat, cari, ganti):
-#     kalimat_baru = ''
-#     kata = ''
-#     for i in range(len(kalimat)):
-#         if kalimat[i] == ' ':
-#             if kata == cari:
-#                 kalimat_baru += ganti
-#             else:
-#                 kalimat_baru += kata
-#             kalimat_baru += ' '
-#             kata = ''
-#         else:
-#             kata += kalimat[i]
-#     if kata == cari:
-#         kalimat_baru += ganti
-#     else:
-#         kalimat_baru += kata
-#     return kalimat_baru
-
-# kalimat = input("Masukkan kalimat: ")
-# cari = input("Masukkan kata yang ingin diganti: ")
-# ganti = input("Masukkan kata pengganti: ")
-
-# kalimat_baru = ganti_kata(kalimat, cari, ganti)
-# print("Kalimat baru: ", kalimat_baru)
-
-
 def ganti_kata(kalimat, cari, ganti):
     kalimat_baru = ''
     for kata in kalimat.split():
